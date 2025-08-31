ef3c9a15b0f42796188ab45dc0b8ee85a54c925f
tommyoshea <audioshea@gmail.com>
2025-08-31 16:16:49 +0100

    Fix invalid redirect fields in next.config.js

--- next.config.js
+++ next.config.js
@@ -17,32 +17,6 @@ const nextConfig = {
         destination: 'https://www.localglobalweb.com/:path*',
         permanent: true,
       },
-      // Redirect any subdomain (except reserved ones) to www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: ':subdomain.localglobalweb.com',
-          },
-        ],
-        // Exclude system/email subdomains
-        beforeFiles: [
-          { type: 'host', value: 'mail.localglobalweb.com', negate: true },
-          { type: 'host', value: 'webmail.localglobalweb.com', negate: true },
-          { type: 'host', value: 'cpanel.localglobalweb.com', negate: true },
-          { type: 'host', value: 'ftp.localglobalweb.com', negate: true },
-        ],
-        destination: 'https://www.localglobalweb.com/:path*',
-        permanent: true,
-      },
-      // Redirect all HTTP traffic to HTTPS
-      {
-        source: '/:path*',
-        protocol: 'http',
-        destination: 'https://www.localglobalweb.com/:path*',
-        permanent: true,
-      },
     ];
   },
   async rewrites() {
