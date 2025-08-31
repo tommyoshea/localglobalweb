f8a344cfdff333ea0080fe26dd096462522541b2
tommyoshea <audioshea@gmail.com>
2025-08-31 16:10:54 +0100

    Update next.config.js configuration

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   async redirects() {
     return [
-      // Redirect root domain to www
+      // Redirect root domain (non-www) to www
       {
         source: '/:path*',
         has: [
@@ -14,6 +14,32 @@ const nextConfig = {
             value: 'localglobalweb.com',
           },
         ],
+        destination: 'https://www.localglobalweb.com/:path*',
+        permanent: true,
+      },
+      // Redirect any subdomain (except reserved ones) to www
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: ':subdomain.localglobalweb.com',
+          },
+        ],
+        // Exclude system/email subdomains
+        beforeFiles: [
+          { type: 'host', value: 'mail.localglobalweb.com', negate: true },
+          { type: 'host', value: 'webmail.localglobalweb.com', negate: true },
+          { type: 'host', value: 'cpanel.localglobalweb.com', negate: true },
+          { type: 'host', value: 'ftp.localglobalweb.com', negate: true },
+        ],
+        destination: 'https://www.localglobalweb.com/:path*',
+        permanent: true,
+      },
+      // Redirect all HTTP traffic to HTTPS
+      {
+        source: '/:path*',
+        protocol: 'http',
         destination: 'https://www.localglobalweb.com/:path*',
         permanent: true,
       },
