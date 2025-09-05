818c8ab33d2073480b5de2058c295c31be87d80c
tommyoshea <audioshea@gmail.com>
2025-09-05 12:22:30 +0100

    Add Google Analytics tracking (AW-17529798525)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import CookieNotification from '@/components/CookieNotification'
 import WhatsAppButton from '@/components/WhatsAppButton'
@@ -87,6 +88,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17529798525"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17529798525');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <ViewportMeta />
         <StructuredData />
